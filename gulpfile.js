3eaace855b27250e5f633eeccfa9765dc86fcdba
bpandof <bpandofdesarrolador@gmail.com>
2024-02-25 11:30:14 +0100

    162. Creando y llamando tareas en Gulp con NPX

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,11 @@
 // npm i -D gulp 
 
 //luego crear un archivo en la raiz del proyecto con nombre
+
+function tarea(done){
+    console.log('mi primer tarea')
+
+    done()
+}
+
+exports.tarea = tarea
