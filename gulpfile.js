da458bc4df14798cd07b61616b3724ae4c554ab9
bpandof <bpandofdesarrolador@gmail.com>
2024-02-25 17:54:22 +0100

    171. Añadiendo Plumber

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,12 @@
 
 const { src,dest,watch } = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done) {  
 
 src('src/scss/**.*.scss') // Identificar el archivo SASS
+    .pipe( plumber() ) //Con esto evito que se detenga el código cada vez que de un error
     .pipe( sass() ) // Compilarlo
     .pipe(dest('build/css')) // Almacenar
 
