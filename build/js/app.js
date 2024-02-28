69d98e72cae27b255f59bafa44ba5485b294cb7b
bpandof <bpandofdesarrolador@gmail.com>
2024-02-28 19:22:21 +0100

    199. Detectando la imagen a la que le damos click para mostrar una versión más grande

--- build/js/app.js
+++ build/js/app.js
@@ -1,25 +1,46 @@
-document.addEventListener('DOMContentLoaded',function(){
-    iniciarApp();
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
 });
 
-function iniciarApp(){
-    crearGaleria();
+function iniciarApp() {
+  crearGaleria();
 }
 
-function crearGaleria(){
-    const galeria = document.querySelector('.galeria-imagenes');
-    
-    for(let i = 1; i<=12;i++){
-        const imagen = document.createElement('picture');
-        imagen.innerHTML = `
+function crearGaleria() {
+  const galeria = document.querySelector(".galeria-imagenes");
+
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement("picture");
+    imagen.innerHTML = `
             <source srcset="build/img/thumb/${i}.avif" type="image/avif" />
             <source srcset="build/img/thumb/${i}.webp" type="image/webp" />
             <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria"/>
         `;
 
-        galeria.appendChild(imagen);
+    galeria.appendChild(imagen);
+    
+    imagen.onclick = function () {
+      mostrarImagen(i);
+      
     }
-    
-    
-    
-};
+  }
+}
+
+function mostrarImagen(id) {
+    console.log(id)
+    const imagen = document.createElement("picture");
+    imagen.innerHTML = `
+            <source srcset="build/img/grande/${id}.avif" type="image/avif" />
+            <source srcset="build/img/grande/${id}.webp" type="image/webp" />
+            <img loading="lazy" width="400" height="600" src="build/img/grande/${id}.jpg" alt="imagen galeria"/>
+        `;
+
+    const overlay = document.createElement('DEV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+}
+
+
